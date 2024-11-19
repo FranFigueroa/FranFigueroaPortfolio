36f714721ba44000a11bf9205245a2bb29fc1cd0
FranFigueroa <70865088+FranFigueroa@users.noreply.github.com>
2024-11-18 21:14:25 -0300

    feat: Hide Projects and edit footer
    Must fix Contact section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,10 +7,10 @@ function Footer() {
   return (
     <footer>
       <div>
-        <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-        <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+        <a href="https://github.com/FranFigueroa" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+        <a href="https://www.linkedin.com/in/franco-figueroa-575696193/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
       </div>
-      <p>A portfolio designed & built by <a href="https://github.com/yujisatojr/react-portfolio-template" target="_blank" rel="noreferrer">Yuji Sato</a> with 💜</p>
+      <p>A portfolio designed & built by <a href="https://github.com/FranFigueroa" target="_blank" rel="noreferrer">Fran Figueroa</a> with 🤍</p>
     </footer>
   );
 }
